777d509c04757707c0d5c131e7a32df1e22e82ca
murad-mia <muradmia901@gmail.com>
2023-06-09 19:12:53 +0600

    project(C++)/Resturant Bill management project

---
+++ Resturant Bill management project/Resturant_Bill_management_project.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void head(){
+    cout<<"\t*****************************************\n";
+    cout<<"\t\t    Warisha FOOD ZONE";
+    cout<<"\n\t*****************************************";
+}
+
+void body(){
+    
+}
+int main(){
+    head();
+ 
+  return 0;
+}
